36ff5cebd38935a97e97098698b4aa5a8fce0b16
jaresj <justice.jares@gmail.com>
2023-02-22 11:59:14 -0800

    formatted code

--- pages/graveyard.js
+++ pages/graveyard.js
@@ -6,7 +6,7 @@ const Graveyard = () => (
     <div className="relative">
       <h1 className="flex justify-center text-white">This Week in the Graveyard</h1>
       <div className="flex justify-center">
-        <iframe class="w-[90%] flex justify-center" title="calendar" src="https://calendar.google.com/calendar/embed?&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showTitle=0&showNav=0&showTz=0&showCalendars=0&showTabs=0&mode=WEEK&showPrint=0&src=Z29pbmcuZ2hvc3Qub3JnYW5pemF0aW9uQGdtYWlsLmNvbQ&color=%23039BE5"/>
+        <iframe className="w-[90%] flex justify-center" title="calendar" src="https://calendar.google.com/calendar/embed?&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showTitle=0&showNav=0&showTz=0&showCalendars=0&showTabs=0&mode=WEEK&showPrint=0&src=Z29pbmcuZ2hvc3Qub3JnYW5pemF0aW9uQGdtYWlsLmNvbQ&color=%23039BE5" />
       </div>
     </div>
     <div className="relative" />
