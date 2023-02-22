57bfb01846c0f82a49697158deba5637cddbb21e
jaresj <justice.jares@gmail.com>
2023-02-22 12:10:46 -0800

    changed timezone for calendar

--- pages/graveyard.js
+++ pages/graveyard.js
@@ -4,9 +4,9 @@ const Graveyard = () => (
   <div className="min-h-screen bg-cover bg-no-repeat bg-fixed bg-center bg-primary-black">
     <Navbar />
     <div className="relative">
-      <h1 className="flex justify-center text-white">This Week in the Graveyard</h1>
+      <h1 className="flex justify-center font-bold md:text-[64px] text-[44px] text-white">This Week in the Graveyard</h1>
       <div className="flex justify-center">
-        <iframe className="w-[90%] flex justify-center" title="calendar" src="https://calendar.google.com/calendar/embed?&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showTitle=0&showNav=0&showTz=0&showCalendars=0&showTabs=0&mode=WEEK&showPrint=0&src=Z29pbmcuZ2hvc3Qub3JnYW5pemF0aW9uQGdtYWlsLmNvbQ&color=%23039BE5" />
+        <iframe className="w-[90%] flex justify-center" title="calendar" src="https://calendar.google.com/calendar/embed?&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&showTitle=0&showNav=0&showTz=0&showCalendars=0&showTabs=0&mode=WEEK&showPrint=0&src=Z29pbmcuZ2hvc3Qub3JnYW5pemF0aW9uQGdtYWlsLmNvbQ&color=%23039BE5" />
       </div>
     </div>
     <div className="relative" />
